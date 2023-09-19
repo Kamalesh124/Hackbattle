2b929cf978d59bb8343fa56b91c07c59832b4a36
kv8gh <vyaskaran1409@gmail.com>
2023-09-20 04:07:04 +0530

    signIn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginPage from './components/LoginPage'
 
+export default function Home(){
+
+const [stage, setStage] = useState("loginPage")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <LoginPage />
 );
-
+}
 
